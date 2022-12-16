cbb512b06fb869f463320839d38dfcdc27046266
Albert Portnoy <albert@albertp.dev>
2022-12-16 17:45:05 -0600

    Replugged Rewrite

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # No nitro upsell
-Removes ALL of Discord's nitro upsells by tricking the client into thinking you have nitro.  Works with Replugged and Powercord.
 
-[Install in Replugged](https://replugged.dev/install?url=asportnoy/powercord-no-nitro-upsell)
+Removes ALL of Discord's nitro upsells by tricking the client into thinking you have nitro. Works
+with Replugged and Powercord.
 
 ## Notes
+
 ⚠️ THIS DOES NOT GIVE YOU ACCESS TO NITRO FEATURES. THOSE ARE RESTRICTED SERVER-SIDE. ⚠️
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { User } from "discord-types/general";
+import { webpack } from "replugged";
+
+let initialValue: number | undefined;
+let user: User | undefined;
+let lastUserId: string | undefined;
+
+export async function start(): Promise<void> {
+  const { addChangeListener, getCurrentUser } = await webpack.waitForModule<{
+    addChangeListener: (listener: () => void) => void;
+    getCurrentUser: () => User | undefined;
+  }>(webpack.filters.byProps("getCurrentUser", "getUser"));
+
+  user = getCurrentUser();
+  if (user) ready();
+
+  addChangeListener(() => {
+    const newUser = getCurrentUser();
+    if (newUser && newUser.id !== lastUserId) {
+      user = newUser;
+      ready();
+    }
+  });
+}
+
+function ready(): void {
+  if (!user) return;
+
+  initialValue = user.premiumType ?? 0;
+  user.premiumType = 2;
+  lastUserId = user.id;
+}
+
+export function stop(): void {
+  if (!user || initialValue === undefined) return;
+  user.premiumType = initialValue;
+}
