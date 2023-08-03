506bc3e942f62b6a77835bb6b903cba575bbe48b
Albert Portnoy <albert@albertp.dev>
2023-08-02 22:17:27 -0500

    Refactor, proper uninjecting, expose real premium type

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,60 @@
 import { User } from "discord-types/general";
 import { webpack } from "replugged";
+import { Store } from "replugged/dist/renderer/modules/common/flux";
 
-let initialValue: number | undefined;
-let user: User | undefined;
+let user: ModifiedUser | undefined;
 let lastUserId: string | undefined;
 
-export async function start(): Promise<void> {
-  const { addChangeListener, getCurrentUser } = await webpack.waitForModule<{
-    addChangeListener: (listener: () => void) => void;
-    getCurrentUser: () => User | undefined;
-  }>(webpack.filters.byProps("getCurrentUser", "getUser"));
+interface ModifiedUser extends User {
+  _realPremiumType?: number;
+}
+
+interface UserStore extends Store {
+  addChangeListener: (listener: () => void) => void;
+  removeChangeListener: (listener: () => void) => void;
+  getCurrentUser: () => ModifiedUser | undefined;
+}
+
+let uninject: (() => void) | undefined;
+let userStore: UserStore | undefined;
+
+export function start(): void {
+  userStore = webpack.getByStoreName<UserStore>("UserStore");
+  if (!userStore) throw new Error("UserStore not found");
+  const { addChangeListener, removeChangeListener, getCurrentUser } = userStore;
 
   user = getCurrentUser();
-  if (user) ready();
+  if (user) ready(user);
 
-  addChangeListener(() => {
+  const onChange = (): void => {
     const newUser = getCurrentUser();
     if (newUser && newUser.id !== lastUserId) {
       user = newUser;
-      ready();
+      ready(user);
     }
-  });
+  };
+
+  addChangeListener(onChange);
+  uninject = () => removeChangeListener(onChange);
 }
 
-function ready(): void {
+function ready(user: ModifiedUser): void {
   if (!user) return;
 
-  initialValue = user.premiumType ?? 0;
+  user._realPremiumType = user.premiumType ?? 0;
   user.premiumType = 2;
   lastUserId = user.id;
 }
 
 export function stop(): void {
-  if (!user || initialValue === undefined) return;
-  user.premiumType = initialValue;
+  uninject?.();
+
+  if (!userStore) return;
+  const { getCurrentUser } = userStore;
+  const user = getCurrentUser();
+  if (!user) return;
+  if (typeof user._realPremiumType === "number") {
+    user.premiumType = user._realPremiumType;
+    delete user._realPremiumType;
+  }
 }
